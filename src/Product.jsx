2fd62688ad312d5857806a3388d8018efd78f48a
Aditya Maurya <adityamaurya5656@gmail.com>
2026-03-29 22:57:29 +0530

    first commit

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,13 +1,18 @@
 import './App.css';
 import './Product.css';
 import ProductTab from './ProductTab';
-function Product ({title,price}) {
-    
+function Product ({title,price,description,}) {
+    let isDiscount = price>3000 ? "5%": "";
+    let styles={backgroundColor: price>30000? "yellow":""};
+    let h3={backgroundColor:"teal"}
+    let p={color:"brown"}
     return (
         
-        <div className='Product'>
-            <h1>{title}</h1> <br/>
-            <h3>Price:{price}</h3>
+        <div className='Product' style={styles}>
+            <h2>{title}</h2> <br/>
+            < h3 style={h3}>Price:{price} </h3>
+            <p style={p}>description:{description}</p>
+            <h4>{isDiscount}</h4>
         </div>
     );
 }
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -3,13 +3,16 @@ import './Product.css';
 
 
 function ProductTab() {
-
-    return (
+    let options = [<li>hii-tech</li>, <li>"durable"</li>, <li>"fast"</li>];
+        return (
 
         <>
-        <Product title="Phone" price={10000}/>
-        <Product title="Laptop" price={40000}/>
-        <Product title="Pen" price={45/3}/>
+        <Product title="Iphone" price={10000} description="This product is good" features={options}/>
+        <Product title="Laptop" price={40000} description="Warranty is for 2 years only"/>
+        <Product title="Pen" price={45/3} description="Made up of Graphite"/>
+        <Product title="Laptop charger" price={4000} description="Warranty up to 2 years"/>
+        <Product/>
+        
         </>
     )
 }
